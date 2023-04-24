619dde41acb9e60109803a9cab1210bb9f54166d
Edwin Cardenas <eacardenase@unal.edu.co>
2023-04-24 17:59:27 -0500

    feat: returning complex objects as json

--- src/main/java/com/eacardenase/Main.java
+++ src/main/java/com/eacardenase/Main.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Objects;
 
 @SpringBootApplication
@@ -16,41 +17,20 @@ public class Main {
 
     @GetMapping("/greet")
     public GreetResponse greet() {
-        return new GreetResponse("Hello from Spring Boot");
+        GreetResponse response = new GreetResponse(
+                "Hello from Spring Boot",
+                List.of("Java", "TypeScript"),
+                new Person("Edwin", 27, 0.00)
+        );
+
+        return response;
     }
 
-//    record GreetResponse(String greet) {
-//    }
-
-    class GreetResponse {
-        private final String greet;
-
-        GreetResponse(String greet) {
-            this.greet = greet;
-        }
-
-        public String getGreet() {
-            return greet;
-        }
-
-        @Override
-        public String toString() {
-            return "GreetResponse{" +
-                    "greet='" + greet + '\'' +
-                    '}';
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            GreetResponse that = (GreetResponse) o;
-            return Objects.equals(greet, that.greet);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(greet);
-        }
+    record Person(String name, int age, double savings) {}
+    record GreetResponse(
+            String greet,
+            List<String> favProgrammingLanguages,
+            Person person
+    ) {
     }
 }
